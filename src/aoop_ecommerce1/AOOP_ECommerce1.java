b44252291b288429806d2c7a84b86301ba067c5e
Jeremy Adam <jeremyadam446@gmail.com>
2018-01-11 00:27:15 +0700

    delete Cart when open app

--- src/aoop_ecommerce1/AOOP_ECommerce1.java
+++ src/aoop_ecommerce1/AOOP_ECommerce1.java
@@ -7,9 +7,11 @@ package aoop_ecommerce1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import net.proteanit.sql.DbUtils;
 
 /**
  *
@@ -20,6 +22,15 @@ import java.sql.Statement;
 public class AOOP_ECommerce1 {
 
     public static void main(String[] args) {
+        Connection con = null;
+        String query = "DELETE FROM CART";
+        try{
+            con = DriverManager.getConnection("jdbc:derby://localhost:1527/TokipedDB");
+            PreparedStatement deleteInit = con.prepareStatement(query);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
        new LoginFrame().setVisible(true);
     }
     
