05678113d2687a8270fd1aa3fed9731c0dce9b26
J . Renael <J . Renael@DESKTOP-A7TNC6R>
2018-01-11 00:01:02 +0700

    add addToCartFunction , Bug Increment CartID

--- src/aoop_ecommerce1/FnbFrame.java
+++ src/aoop_ecommerce1/FnbFrame.java
@@ -5,6 +5,13 @@
  */
 package aoop_ecommerce1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author J . Renael
@@ -14,6 +21,10 @@ public class FnbFrame extends javax.swing.JFrame {
     /**
      * Creates new form FnbFrame
      */
+    Connection con = null ;
+    Statement state = null;
+    ResultSet res = null;
+    
     public FnbFrame() {
         initComponents();
     }
@@ -31,8 +42,14 @@ public class FnbFrame extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
+        spinnerAlmond = new javax.swing.JSpinner();
+        jLabel4 = new javax.swing.JLabel();
+        btnCartAlmond = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
+        spinnerMacaron = new javax.swing.JSpinner();
+        jLabel5 = new javax.swing.JLabel();
+        btnCartMacaron = new javax.swing.JButton();
         btnHome = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -43,13 +60,30 @@ public class FnbFrame extends javax.swing.JFrame {
 
         jLabel2.setText("Kacang Almond");
 
+        jLabel4.setText("Quantity");
+
+        btnCartAlmond.setText("Add to Cart");
+        btnCartAlmond.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCartAlmondActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(104, Short.MAX_VALUE)
-                .addComponent(jLabel2)
+                .addGap(35, 35, 35)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnCartAlmond)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jLabel2)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                            .addComponent(spinnerAlmond, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(25, 25, 25))))
                 .addGap(101, 101, 101))
         );
         jPanel2Layout.setVerticalGroup(
@@ -57,26 +91,56 @@ public class FnbFrame extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(jLabel2)
-                .addContainerGap(160, Short.MAX_VALUE))
+                .addGap(61, 61, 61)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(spinnerAlmond, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnCartAlmond)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jLabel3.setText("Macaron");
+
+        jLabel5.setText("Quantity");
+
+        btnCartMacaron.setText("Add to Cart");
+        btnCartMacaron.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCartMacaronActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(113, 113, 113)
-                .addComponent(jLabel3)
-                .addContainerGap(135, Short.MAX_VALUE))
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGap(113, 113, 113)
+                        .addComponent(jLabel3))
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jLabel5)
+                        .addGap(56, 56, 56)
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnCartMacaron)
+                            .addComponent(spinnerMacaron, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(80, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(19, 19, 19)
                 .addComponent(jLabel3)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(61, 61, 61)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(spinnerMacaron, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addGap(18, 18, 18)
+                .addComponent(btnCartMacaron)
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
         btnHome.setText("Home");
@@ -97,9 +161,9 @@ public class FnbFrame extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(90, 90, 90)
+                .addGap(104, 104, 104)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 235, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 209, Short.MAX_VALUE)
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(88, 88, 88))
         );
@@ -114,7 +178,7 @@ public class FnbFrame extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(157, Short.MAX_VALUE))
+                .addContainerGap(143, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -125,7 +189,10 @@ public class FnbFrame extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -136,6 +203,69 @@ public class FnbFrame extends javax.swing.JFrame {
         this.setVisible(false);
         new HomePageFrame().setVisible(true);
     }//GEN-LAST:event_btnHomeActionPerformed
+
+    private void btnCartAlmondActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCartAlmondActionPerformed
+        // TODO add your handling code here:
+        try {
+            int id = 0 ;
+            con = DriverManager.getConnection("jdbc:derby://localhost:1527/TokipedDB");
+            String query = "SELECT MAX(CartID) FROM Cart GROUP BY NamaBarang , Jumlah , Harga ";
+            state = con.createStatement();
+            res = state.executeQuery(query);
+            if (res.next()) {
+                 id = res.getInt(1) ;
+                 id+=3;
+            }
+            
+            String namaBarang = "Kacang Almond" ;
+            Integer quantity = (Integer)spinnerAlmond.getValue();
+            int harga = quantity * 3000 ;
+            System.out.println(id);
+            PreparedStatement add = con.prepareStatement("insert Into Cart values (?,?,?,?)");
+            add.setInt(1, id);
+            add.setString(2, namaBarang);
+            add.setInt(3, quantity);
+            add.setInt(4, harga);
+            int row = add.executeUpdate();
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        this.setVisible(false);
+        new HomePageFrame().setVisible(true);
+    }//GEN-LAST:event_btnCartAlmondActionPerformed
+
+    private void btnCartMacaronActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCartMacaronActionPerformed
+                try {
+            int id = 0 ;
+            con = DriverManager.getConnection("jdbc:derby://localhost:1527/TokipedDB");
+            String query = "SELECT MAX(CartID) FROM Cart GROUP BY NamaBarang , Jumlah , Harga ";
+            state = con.createStatement();
+            res = state.executeQuery(query);
+            if (res.next()) {
+                 id = res.getInt(1) ;
+                 id+=3;
+            }
+            String namaBarang = "Macaron" ;
+            Integer q = (Integer)spinnerMacaron.getValue();
+            int harga = q * 5000 ;
+            System.out.println(id);
+            PreparedStatement add = con.prepareStatement("insert Into Cart values (?,?,?,?)");
+            add.setInt(1, id);
+            add.setString(2, namaBarang);
+            add.setInt(3, q);
+            add.setInt(4, harga);
+            int row = add.executeUpdate();
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        this.setVisible(false);
+        new HomePageFrame().setVisible(true); 
+
+    }//GEN-LAST:event_btnCartMacaronActionPerformed
 
     /**
      * @param args the command line arguments
@@ -173,12 +303,18 @@ public class FnbFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCartAlmond;
+    private javax.swing.JButton btnCartMacaron;
     private javax.swing.JButton btnHome;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JSpinner spinnerAlmond;
+    private javax.swing.JSpinner spinnerMacaron;
     // End of variables declaration//GEN-END:variables
 }
